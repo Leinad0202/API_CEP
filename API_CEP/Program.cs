86800f4a575363731e8ed3d3083e48e6b8d25a51
Daniel <daniel.melicio81@gmail.com>
2025-12-08 20:55:35 -0300

    Fix Docker startup DB race with retry before migrations

--- API_CEP/Program.cs
+++ API_CEP/Program.cs
@@ -24,15 +24,36 @@ builder.Services.AddScoped<CepService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    var retry = 0;
+    while (retry < 10)
+    {
+        try
+        {
+            db.Database.OpenConnection();
+            db.Database.CloseConnection();
+            break;
+        }
+        catch
+        {
+            retry++;
+            Thread.Sleep(2000);
+        }
+    }
+
+    db.Database.Migrate();
+}
+
+
 // Configure the HTTP request pipeline
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "API CEP v1");
 });
-
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
